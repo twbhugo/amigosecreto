bbca87349cdf078e283436d54e4fd80ac64ab986
twbhugo <115596903+twbhugo@users.noreply.github.com>
2025-12-12 12:41:14 -0600

    fixing CSRF2

--- amigosecreto/settings.py
+++ amigosecreto/settings.py
@@ -41,6 +41,13 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+# CSRF_TRUSTED_ORIGINS
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-a6c38.up.railway.app/',
+    'https://navidad.twbapps.com/'
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
